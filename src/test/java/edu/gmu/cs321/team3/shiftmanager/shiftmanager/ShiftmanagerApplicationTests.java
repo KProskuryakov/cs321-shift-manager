75a3fdc47ef8daaaeac6cd28d453dd9e4c39349b
Kostyantyn Proskuryakov <2399977+KProskuryakov@users.noreply.github.com>
2020-02-23 18:54:14 -0500

    Spring initializr scaffolding

---
+++ src/test/java/edu/gmu/cs321/team3/shiftmanager/shiftmanager/ShiftmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.gmu.cs321.team3.shiftmanager.shiftmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
