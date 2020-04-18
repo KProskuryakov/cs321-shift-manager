8020a644e2404cca734293b814c16c76a11e6220
Kostyantyn Proskuryakov <2399977+KProskuryakov@users.noreply.github.com>
2020-04-18 16:31:17 -0400

    Jchoi49 (#2)
    * Change
    
    Change
    
    * Org_registration
    
    org
    
    Co-authored-by: jchoi7118 <61396276+jchoi7118@users.noreply.github.com>

---
+++ src/main/java/edu/gmu/cs321/team3/shiftmanager/users/UserValidator.java
@@ -0,0 +1,38 @@
+package edu.gmu.cs321.team3.shiftmanager.users;
+
+import edu.gmu.cs321.team3.shiftmanager.users.User;
+import edu.gmu.cs321.team3.shiftmanager.config.MyUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+@Component
+public class UserValidator implements Validator {
+    @Autowired
+    private MyUserService userService;
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return User.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        User user = (User) o;
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "NotEmpty");
+        if (user.getEmail().length() < 8 || user.getEmail().length() > 32) {
+            errors.rejectValue("email", "Length.userForm.email");
+        }
+        if (userService.findByEmail(user.getEmail()) != null) {
+            errors.rejectValue("email", "Duplicate.userForm.email");
+        }
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "NotEmpty");
+        if (user.getPassword().length() < 8 || user.getPassword().length() > 32) {
+            errors.rejectValue("password", "Length.userForm.password");
+        }
+    }
+}
