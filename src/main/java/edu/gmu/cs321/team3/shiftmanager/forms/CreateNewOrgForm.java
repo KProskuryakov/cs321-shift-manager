6496da10b91e8f5418f456d2393bf8cef7f91115
jchoi7118 <61396276+jchoi7118@users.noreply.github.com>
2020-04-18 18:05:24 -0400

    Update CreateNewOrgForm.java
    Forgot to save this form

--- src/main/java/edu/gmu/cs321/team3/shiftmanager/forms/CreateNewOrgForm.java
+++ src/main/java/edu/gmu/cs321/team3/shiftmanager/forms/CreateNewOrgForm.java
@@ -1,10 +1,14 @@
 package edu.gmu.cs321.team3.shiftmanager.forms;
 
+import edu.gmu.cs321.team3.shiftmanager.orgs.UniqueOrgConstraint;
+
 public class CreateNewOrgForm {
 
+	@UniqueOrgConstraint
 	private String name;
 	private String information;
 
+	
 	public String getName() {
 		return name;
 	}
