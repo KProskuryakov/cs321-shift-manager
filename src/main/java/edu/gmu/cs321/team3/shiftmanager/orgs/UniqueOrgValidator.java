c3e639e84a5f346ec7189829eccd7d8c2b20654e
jchoi7118 <61396276+jchoi7118@users.noreply.github.com>
2020-04-18 17:55:35 -0400

    Org_name_validation
    UniqueOrgConstraint

---
+++ src/main/java/edu/gmu/cs321/team3/shiftmanager/orgs/UniqueOrgValidator.java
@@ -0,0 +1,21 @@
+package edu.gmu.cs321.team3.shiftmanager.orgs;
+
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UniqueOrgValidator implements ConstraintValidator<UniqueOrgConstraint, String> {
+
+    @Autowired
+    private OrgService orgService;
+
+    @Override
+    public boolean isValid(String name, ConstraintValidatorContext context) {
+        return orgService.isNameUnique(name);
+    }
+
+}
