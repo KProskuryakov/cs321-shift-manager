cb06f78f288e613a04d3965032fb4b45035b927f
Kostyantyn Proskuryakov <2399977+KProskuryakov@users.noreply.github.com>
2020-05-04 19:01:16 -0400

    Changed webmvctest to springboottest

--- src/test/java/edu/gmu/cs321/team3/shiftmanager/WebLayerTest.java
+++ src/test/java/edu/gmu/cs321/team3/shiftmanager/WebLayerTest.java
@@ -1,7 +1,5 @@
 package edu.gmu.cs321.team3.shiftmanager;
 
-
-
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -11,12 +9,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 
 
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class WebLayerTest {
 
 	@Autowired
