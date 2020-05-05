5afe37d0ddf23fbb7c7274598dbacf7d235487ab
jchoi7118 <61396276+jchoi7118@users.noreply.github.com>
2020-05-04 23:09:43 -0400

    UserServiceTest
    UserServiceTest

--- src/test/java/edu/gmu/cs321/team3/shiftmanager/users/UserServiceTest.java
+++ src/test/java/edu/gmu/cs321/team3/shiftmanager/users/UserServiceTest.java
@@ -1,10 +1,13 @@
 package edu.gmu.cs321.team3.shiftmanager.users;
 
+import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import edu.gmu.cs321.team3.shiftmanager.forms.CreateAccountForm;
+
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 import javax.transaction.Transactional;
@@ -17,7 +20,6 @@ public class UserServiceTest {
 
     @Autowired
     private UserRepository userRepo;
-
 
     @Test
     @Transactional
@@ -33,11 +35,34 @@ public class UserServiceTest {
 
         assertThat(userRepo.findByEmail("test1@test.com")).isNotNull();
     }
+
+
     
     @Test
     @Transactional
-	public void z() throws Exception {
-        assertThat(userRepo.findByEmail("test1@test.com")).isNull();
+	public void testUniqueEmail() throws Exception {
+        assertThat(userService.isEmailUnique("test1@test.com"));
+
+        CreateAccountForm userForm = new CreateAccountForm();
+        userForm.setFname("Michael");
+        userForm.setLname("Scott");
+        userForm.setEmail("test1@test.com");
+        userForm.setPassword("pass");
+        userForm.setReEnteredPassword("pass");
+
+        userService.registerNewUser(userForm);
+
+        assertThat(!(userService.isEmailUnique("test1@test.com")));
     }
+    
+    @Test
+    @Transactional
+	public void testEnsureManager() throws Exception {
+        
+
+        assertThatCode(() -> { userService.userForEmail("test1@test.com"); }).doesNotThrowAnyException();
+
+    }
+
 
 }
